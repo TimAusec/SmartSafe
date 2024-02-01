8dbc9ef1b6a8ed32d7436a090a1b93c321d8b2b3
TimAusec <114826144+TimAusec@users.noreply.github.com>
2024-02-01 16:58:51 -0500

    Keypad Development
    Working on converting input array to a key type.
    Included MSP432P4111 files folder
    Changed some plan documents

--- ece230projectFinalAusecTTranT/KeyPad.h
+++ ece230projectFinalAusecTTranT/KeyPad.h
@@ -20,9 +20,6 @@ extern "C"
 #include "msp.h"
 #include "stdbool.h"
 
-uint16_t SCAN_OUTPUT_SEQUENCE[8]  = {0b01111111,0b10111111,0b11011111
-                                 ,0b11101111,0b11110111,0b11111011,0b11111101,0b11111110};
-
 typedef enum _key {one, two, three, A, four, five, six, B, seven, eight, nine, C, asterisk, zero, pound, D} key;
 
 #define CORRECT_CODE;
@@ -41,5 +38,4 @@ extern void IRQ();
 
 extern void HandleButtonPressed();
 
-
-#endif
+extern key ConvertInputToKey(char* input);
