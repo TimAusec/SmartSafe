500645a9448f87c18658a1fe3f12cad7165943b2
rhit-tysonla <95252653+rhit-tysonla@users.noreply.github.com>
2024-02-01 11:26:40 -0500

    Fixed errors so project can build

--- ece230projectFinalAusecTTranT/KeyPad.c
+++ ece230projectFinalAusecTTranT/KeyPad.c
@@ -7,8 +7,11 @@
 #include <msp.h>
 #include <stdint.h>
 #include "KeyPad.h"
-uint8_t SCAN_OUTPUT_SEQUENCE[8]={0b01111111,0b10111111,0b11011111
-                                 ,0b11101111,0b11110111,0b11111011,0b11111101,0b11111110};
+#include "stdbool.h"
+
+//uint8_t SCAN_OUTPUT_SEQUENCE[8]={0b01111111,0b10111111,0b11011111
+//                                 ,0b11101111,0b11110111,0b11111011,0b11111101,0b11111110};
+
 #define KEYPAD_PORT P2
 
 uint16_t currentCode[4]={zero,zero,zero,zero};
@@ -47,5 +50,4 @@ void IRQ(){
 void HandleButtonPressed(){
     key pressed=GetKeyPressed();
     SaveKeyToCode(pressed);
-    if()
 }
