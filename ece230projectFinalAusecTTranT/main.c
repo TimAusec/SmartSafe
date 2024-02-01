5dcd81c26a2b9d30e7f3b7b5e6be29f553a0023b
rhit-tysonla <95252653+rhit-tysonla@users.noreply.github.com>
2024-02-01 11:20:36 -0500

    Main

--- ece230projectFinalAusecTTranT/main.c
+++ ece230projectFinalAusecTTranT/main.c
@@ -14,8 +14,15 @@
 #include <msp.h>
 #include <stdint.h>
 #include "KeyPad.h"
+#include "LED.h"
 
 void main(void)
 {
+
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+	ConfigureLED();
+	    led1on();
+
+
 }
