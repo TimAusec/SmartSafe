6573e92a20019370450e161268af218103e156ab
TimAusec <114826144+TimAusec@users.noreply.github.com>
2024-02-01 20:11:44 -0500

    KeyPad Progress
    All that is left is to implement handler and clean up

--- ece230projectFinalAusecTTranT/KeyPad.c
+++ ece230projectFinalAusecTTranT/KeyPad.c
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /*
  * KeyPad.c
  *
@@ -77,3 +78,185 @@ void HandleButtonPressed()
     key pressed = GetKeyPressed();
     SaveKeyToCode(pressed);
 }
+=======
+/*
+ * KeyPad.c
+ *
+ *  Created on: Jan 31, 2024
+ *      Author: ausectj
+ */
+#include <msp.h>
+#include <stdint.h>
+#include "KeyPad.h"
+
+typedef enum _bool
+{
+    false, true
+} bool;
+typedef enum _key
+{
+    one,
+    two,
+    three,
+    A,
+    four,
+    five,
+    six,
+    B,
+    seven,
+    eight,
+    nine,
+    C,
+    asterisk,
+    zero,
+    pound,
+    D,
+    INVALID
+} key;
+
+/*Define ports and pins*/
+#define KEYPAD_PORT P2
+#define OUTPUT_PINS BIT7|BIT6|BIT5|BIT4
+#define INPUT_PINS  BIT3|BIT2|BIT1|BIT0
+
+/*Define Codes*/
+const key CORRECT_CODE[] = { zero, zero, zero, one };
+const uint8_t SCAN_OUTPUT_SEQUENCE[5] = { BIT7, BIT6, BIT5, BIT4, 0 }; //0 is end bit
+const int CODE_LENGTH = sizeof(CORRECT_CODE) / sizeof(CORRECT_CODE[0]);
+
+key currentCode[CODE_LENGTH] = { zero, zero, zero, zero };
+
+/*Define Hardware Requirements*/
+const int NUMBER_OF_ROWS = 4;
+const int NUMBER_OF_COLUMNS = 4;
+
+/*Define Security Threshold*/
+const int MAX_TRIES_ALLOWED = 4;
+const int currentTriesCount = 0;
+
+void ConfigKeyPad()
+{ //see section 12.4 of the technical reference manual
+    KEYPAD_PORT->DIR |= OUTPUT_PINS | (~INPUT_PINS ); //set output pins to output mode
+    KEYPAD_PORT->DIR &= ~INPUT_PINS; //set input pins to input mode
+    KEYPAD_PORT->REN |= ~(OUTPUT_PINS) | INPUT_PINS; //enable pullup resistors for input pins
+    KEYPAD_PORT->SEL0 |= ~(OUTPUT_PINS | INPUT_PINS ); //primary function for all pins
+    KEYPAD_PORT->SEL1 |= ~(OUTPUT_PINS | INPUT_PINS ); //primary function for all pins
+    KEYPAD_PORT->IE &= ~(OUTPUT_PINS); //disable interrupts on output pins
+    KEYPAD_PORT->IE |= INPUT_PINS;//enable interrupts on input pins
+    KEYPAD_PORT->IES |= ~(OUTPUT_PINS) | INPUT_PINS; //set input interrupts with a low-to-high transition
+    KEYPAD_PORT->OUT |= OUTPUT_PINS; //set the output pins high to start reading
+}
+
+key ConvertInputToKey(char input[])
+{
+    int index;
+    key key;
+    for (index = 0; index < NUMBER_OF_ROWS - 1; index++)
+    {
+        switch (input[index])
+        {
+        case BIT0 :
+            key = NUMBER_OF_COLUMNS * index;
+            break;
+        case BIT1 :
+            key = NUMBER_OF_COLUMNS * index + 1;
+            break;
+        case BIT2 :
+            key = NUMBER_OF_COLUMNS * index + 2;
+            break;
+        case BIT3 :
+            key = NUMBER_OF_COLUMNS * index + 3;
+            break;
+        default:
+            //Invalid input
+            break;
+        }
+    }
+    return key;
+}
+
+key GetKeyPressed()
+{
+    int currentRow;
+    char input[NUMBER_OF_ROWS];
+    key inputKey;
+
+    KEYPAD_PORT->OUT |= SCAN_OUTPUT_SEQUENCE[0];
+    input[0] = KEYPAD_PORT->IN;
+    for (currentRow = 1; currentRow <= NUMBER_OF_ROWS - 1; currentRow++)
+    {
+        KEYPAD_PORT->OUT &= ~SCAN_OUTPUT_SEQUENCE[currentRow - 1]; //pull previous bit down
+        KEYPAD_PORT->OUT |= SCAN_OUTPUT_SEQUENCE[currentRow]; //pull row up
+        input[currentRow] = KEYPAD_PORT->IN;
+    }
+    inputKey = ConvertInputToKey(input);
+    return inputKey;
+}
+
+void ClearCode(int* currentIndex)
+{
+    int index;
+    *currentIndex=0;
+    for (index = 0; index < CODE_LENGTH - 1; index++)
+    {
+        currentCode[index] = INVALID;
+    }
+}
+
+void SaveKeyToCode(key key)
+{
+    static int currentIndex = 0; //initializes only once
+    if (currentIndex % CODE_LENGTH == 0)
+    {
+        ClearCode(&currentIndex);
+    }
+    currentCode[currentIndex] = key;
+    currentIndex = (currentIndex + 1);
+}
+
+bool IsCorrectCode()
+{
+    int index;
+    key match[];
+    bool truth;
+    for (index = 0; index < CODE_LENGTH - 1; index++)
+    {
+        match[index] = (currentCode[index] == CORRECT_CODE[index]); //check each entry
+    }
+
+    truth = match[0];
+    for (index = 1; index < CODE_LENGTH - 1; index++)
+    {
+        truth &= match[index]; //if all entries are 1, true. if any entry is 0, false.
+    }
+    return truth;
+}
+
+bool IsExceededMaxTries()
+{
+    if (currentTriesCount > MAX_TRIES_ALLOWED)
+    {
+        return true;
+    }
+    return false;
+}
+
+void PORT2_IRQHandler() //Check startup_msp432p4111 file
+{
+//TODO: check if button press caused interrupt
+    HandleButtonPressed();
+}
+
+void HandleButtonPressed()
+{
+//TODO: Implement
+    currentTriesCount++;
+    key pressed = GetKeyPressed();
+    SaveKeyToCode(pressed);
+}
+
+void SetAllOutputPinsHigh()
+{
+    KEYPAD_PORT->OUT |= OUTPUT_PINS;
+}
+>>>>>>> Stashed changes
--- ece230projectFinalAusecTTranT/KeyPad.h
+++ ece230projectFinalAusecTTranT/KeyPad.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /*
  * KeyPad.h
  *
@@ -39,3 +40,69 @@ extern void IRQ();
 extern void HandleButtonPressed();
 
 extern key ConvertInputToKey(char* input);
+=======
+/*
+ * KeyPad.h
+ *
+ *  Created on: Jan 31, 2024
+ *      Author: ausectj
+ */
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+//*****************************************************************************
+//
+// If building with a C++ compiler, make all of the definitions in this header
+// have a C binding.
+//
+//*****************************************************************************
+#ifdef __cplusplus
+extern "C"
+
+#endif
+#include "msp.h"
+
+typedef enum _bool
+{
+false, true
+} bool;
+typedef enum _key
+{one,
+two,
+three,
+A,
+four,
+five,
+six,
+B,
+seven,
+eight,
+nine,
+C,
+asterisk,
+zero,
+pound,
+D,
+INVALID
+} key;
+
+extern void ConfigKeyPad();
+
+extern key ConvertInputToKey(char input[]);
+
+extern key GetKeyPressed();
+
+extern void SaveKeyToCode(key key);
+
+extern void ClearCode();
+
+extern bool IsCorrectCode();
+
+extern bool IsExceededMaxTries();
+
+extern void IRQ();
+
+extern void HandleButtonPressed();
+
+extern void SetAllOutputPinsHigh();
+>>>>>>> Stashed changes
