2214c41385564e7278bb55d6e863cae9b4353b46
TimAusec <114826144+TimAusec@users.noreply.github.com>
2024-02-06 21:39:07 -0500

    Implemented Python Code to Write Current Time to a File

---
+++ ece230projectFinalAusecTTranT/Project Code/currentimetest.py
@@ -0,0 +1,4 @@
+import datetime
+f= open('C://Users//ausectj//git//SmartSafe//ece230projectFinalAusecTTranT//CurrentTime.txt','w')
+f.write(str(datetime.datetime.now()))
+print(datetime.datetime.now())
