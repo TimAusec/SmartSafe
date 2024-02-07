5ba1d738dceb5686f9e1835533fcfbcdca184629
rhit-tysonla <95252653+rhit-tysonla@users.noreply.github.com>
2024-02-07 16:31:07 -0500

    LED blinking

--- ece230projectFinalAusecTTranT/Project Code/LED.c
+++ ece230projectFinalAusecTTranT/Project Code/LED.c
@@ -15,6 +15,11 @@ void ConfigureLEDs(void)
         LED_PORT->SEL1 &= ~(GREEN_LED_PIN | RED_LED_PIN);
         LED_PORT->DIR |= (GREEN_LED_PIN | RED_LED_PIN);
         LED_PORT->OUT &= ~(GREEN_LED_PIN | RED_LED_PIN);                       //  LEDs start off
+
+        TIMER_A1->CCR[2] = QUARTER_NOTE;
+                TIMER_A1->CCTL[2] = 0x0010;                                     // Configures CCR2 for Compare mode with interrupt enabled (no output mode - 0)
+                TIMER_A1->CTL = 0b000100000110;                                 //Configures stop mode initially
+                NVIC->ISER[0] |= (1 << (TA1_N_IRQn));
 }
 
 void TA1_N_IRQHandler(void)
@@ -38,7 +43,7 @@ void TA1_N_IRQHandler(void)
 }
 
 void GreenLEDOn() {
-    LED_PORT->OUT |= GREEN_LED_PIN;
+    TIMER_A1->CTL = 0b000100010110;                                  // Configures Timer_A1 in Up Mode with interrupt enabled
 }
 
 void RedLEDOn() {
