8ecdd99f1768c1181e44d4dc7b2cfdeeeb93a90d
rhit-tysonla <95252653+rhit-tysonla@users.noreply.github.com>
2024-02-15 22:44:38 -0500

    Keypad.h comments

--- ece230projectFinalAusecTTranT/Project Code/KeyPad.h
+++ ece230projectFinalAusecTTranT/Project Code/KeyPad.h
@@ -22,32 +22,131 @@ extern "C"
 #include "main.h"
 #include "enums.h"
 
+/*
+ * \brief Configures the keypad
+ * Sets input pins (P4.0-P4.3) to inputs and output pins (P4.4-P4.7) to outputs
+ * Enables interrupts on input pins and disables them on output pins
+ * Sets flags to appropriate start conditions
+ *
+ * Modified P4.0-P4.3 DIR, SEL0, and SEL1 registers
+ * Modified P4.4-P4.7 DIR, SEL0, and SEL1 registers
+ *
+ * \return None
+ */
 extern void ConfigKeyPad(void);
-//TODO: Document
+
+/*
+ * \brief Converts keypad inputs to its associated enum key
+ * Sets the key to the appropriate value based on what keypad is pressed
+ *
+ * \return Key
+ */
 extern key ConvertInputToKey(char input[]);
-//TODO: Document
+
+/*
+ * \brief Determines which row and column the button press came from to translate into a key
+ * Sets outputs to high
+ * Reads input values from the keypad and stores them in input array
+ * Calls convertInputToKey and inputs the updated input array to convert Input to keys
+ *
+ * Modified P4IN & P4OUT
+ *
+ * \return None
+ */
 extern key GetKeyPressed(void);
-//TODO: Document
+
+/*
+ * \brief Sets all entries in the currentCode array to INVALID/NULL
+ * If tries exceeded when this is called, it resets the access attempts counter
+ *
+ * Modified triesExceededFlag, codeClearedFlag, and openCodeFlag
+ *
+ * \return None
+ */
 extern void ClearCode(void);
-//TODO: Document
+
+/*
+ * \brief Saves the current input key into the currentCode array
+ *
+ * Modified codeClearedFlag and triesExceededFlag
+ *
+ * \return None
+ */
 extern void SaveKeyToCode(key key);
-//TODO: Document
+
+/*
+ * \brief Checks each entry in the currentCode array to see if the correct code has been inputted
+ *
+ * Modified match array, bool truth, openCodeFlag, and wrongCodeFlag
+ */
 extern void CalculateIsOpenCode(void);
-//TODO: Document
+
+/*
+ * \brief Handles the operation of the above functions
+ * Determines if a key has been pressed that is not the stop key has been pressed
+ * If it's a valid key, call SaveKeyToCode and CalculateIsOpenCode
+ * Also handles the stop and close keys
+ *
+ * Modified stopCodeFlag, closeCodeFlag, and wrongCodeFlag
+ *
+ * \return None
+ */
 extern void HandleKeyPadButtonValue(key pressed);
-//TODO: Document
+
+/*
+ * \brief Handles keypad button presses
+ * Calls handleKeyPadButtonValue once pressed and Waits for button to be released
+ * Prints key to the console
+ *
+ * \return None
+ */
 extern void HandleKeyPadButtonPressed(void);
-//TODO: Document
+
+/*
+ * \brief Returns CloseCodeFlag
+ *
+ * \return bool
+ */
 extern bool GetCloseCodeFlag(void);
-//TODO: Document
+
+/*
+ * \brief Returns StopCodeFlag
+ *
+ * \return bool
+ */
 extern bool GetStopCodeFlag(void);
-//TODO: Document
+
+/*
+ * \brief Returns TriesExceededFlag
+ *
+ * \return bool
+ */
 extern bool GetTriesExceededFlag(void);
-//TODO: Document
+
+/*
+ * \brief Returns OpenCodeFlag
+ *
+ * \return bool
+ */
 extern bool GetOpenCodeFlag(void);
-//TODO: Document
+
+/*
+ * \brief Returns CurrentTriesCount
+ *
+ * \return int
+ */
 extern int GetCurrentTriesCount();
 
+/*
+ * \brief Returns CurrentIndex
+ *
+ * \return int
+ */
 extern int GetCurrentCodeIndex();
 
+/*
+ * \brief Returns WrongCodeFlag
+ *
+ * \return bool
+ */
 extern bool GetWrongCodeFlag();
